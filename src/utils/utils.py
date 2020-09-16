3c902e6ee84f5187e9203904534ca82efef528c4
nphilipp <philipp.nikolaus90@gmail.com>
2020-09-16 15:35:17 +0200

    changed logging util

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -10,40 +10,76 @@ def make_dirs_checked(directory):
         os.makedirs(directory, exist_ok=True)
 
 
-def get_logger(log_path=None):
-    logger = logging.getLogger()
+def get_logger(log_path: Union[str, None] = None, force: bool = True) -> logging.Logger:
+    """[summary]
+
+    Args:
+        log_path (Union[str, None], optional): [description]. Defaults to None.
+        force (bool, optional): [description]. Defaults to True.
+
+    Returns:
+        logging.Logger: [description]
+    """
+    logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
 
-    console_formatter = colorlog.ColoredFormatter(
-        "%(log_color)s%(message)s",
+    # define colorlog
+    stream_formatter = colorlog.ColoredFormatter(
+        "%(log_color)s%(asctime)s [%(threadName)s] [%(levelname)s]    "
+        "%(reset)s%(blue)s%(message)s",
+        datefmt=None,
+        reset=True,
         log_colors={
-            "DEBUG": "bold_cyan",
-            "INFO": "bold_green",
-            "WARNING": "bold_yellow",
-            "ERROR": "bold_red",
-            "CRITICAL": "white,bg_red",
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red",
         },
     )
 
     # Logging to console
     stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(
-        logging.Formatter("%(asctime)s:%(levelname)s: %(message)s")
-    )
-    stream_handler.setFormatter(console_formatter)
+    stream_handler.setFormatter(stream_formatter)
     logger.addHandler(stream_handler)
 
-    # Logging to a file
-    if not log_path is None:
+    # logging to file
+    if log_path is not None:
+        # check for existing file handlers:
+        for handler in logger.handlers:
+            if type(handler) is logging.FileHandler:
+                if force:
+                    logger.info("Removing existing FileHandler:")
+                    logger.info(f"{getattr(handler,'baseFilename')}")
+                    logger.removeHandler(handler)
+                else:
+                    logger.info("Existing FileHandler found:")
+                    logger.info(f"Logging to {getattr(handler,'baseFilename')} instead")
+
+        # create log_path directory
         make_dirs_checked(log_path)
 
-        ts = datetime.datetime.now().strftime("%Y%m%d-T%H:%M:%S")
-        log_file = f"{'.'.join(sys.argv[0].split('/')[-3:])}-{ts}.log"
+        # create log filename
+        ts = datetime.now().strftime("%Y%m%d-T%H:%M:%S")
+        if "__file__" in locals():
+            if "src" in sys.argv[0]:
+                ts = datetime.now().strftime("%Y%m%d-T%H:%M:%S")
+                path_split = sys.argv[0].split("/")
+                src_idx = path_split.index("src")
+                filename = f"{'.'.join(path_split[src_idx:])}-{ts}.log"
+            else:
+                filename = f"{__file__}-{ts}.log"
+        else:
+            filename = f"terminal-{ts}.log"
+
+        # create filehandler
         file_handler = logging.FileHandler(
-            f"{log_path}/{log_file}", mode="a", encoding=None, delay=False
+            f"{log_path}/{filename}", mode="a", encoding=None, delay=False
         )
         file_handler.setFormatter(
-            logging.Formatter("%(asctime)s:%(levelname)s: %(message)s")
+            logging.Formatter(
+                "%(asctime)s [%(threadName)s] [%(levelname)s] %(message)s"
+            )
         )
         logger.addHandler(file_handler)
 
